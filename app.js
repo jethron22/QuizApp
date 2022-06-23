275bb582c9f316e85c4b0ef8248b69b8bf88992a
cedric karungu <83704005+Cedric921@users.noreply.github.com>
2022-06-23 15:33:55 +0200

    refactoring de l'arrangement du code
    veillez voir d'abord les elements que tu as selectionnee, certains sont dupliquees

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const container = document.querySelector(".container");
 const question = document.querySelector("#container2");
+const questionSecond = document.querySelector(".container-question");
+ const questionTree = document.querySelector(".container-question1");
 
 console.log(container, question);
 const container0 = document.querySelector("#container0");
 
 const container2 = document.querySelector("#container2");
-container2.addEventListener("submit", (e) => {
-    e.preventDefault()
-    question.style.display = 'block'
-    container.style.display = 'none'
-})
-const questionSecond = document.querySelector(".container-question");
+
+
+
 container.addEventListener("submit", (e) => {
     e.preventDefault()
     questionSecond.style.display = 'block'
@@ -18,11 +17,17 @@ container.addEventListener("submit", (e) => {
     console(questionSecond)
 
 })
- const questionTree = document.querySelector(".container-question1");
+
  container3.addEventListener("submit", (e) => {
     e.preventDefault()
     questionTree.style.display ='block'
     questionSecond.style.display = 'none'
  })
+
+container2.addEventListener("submit", (e) => {
+    e.preventDefault()
+    question.style.display = 'block'
+    container.style.display = 'none'
+})
 
 
