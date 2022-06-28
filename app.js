771d2687dd6f0d425c66d683c8361a629f7fc425
Jéthron Kashira <jethronbwira@gmail.com>
2022-06-28 13:33:38 +0200

    final refactor: remove all container and adding new container

--- app.js
+++ app.js
@@ -1,63 +1,71 @@
 const container = document.querySelector(".container");
-const question = document.querySelector("container2");
 
+const containerQuestion = document.querySelector(".container-question");
 
-const containerQuestion1 = document.querySelector(".container-question");
+const buttonOps = document.querySelector(".button-ops");
+const buttonSuivant = document.querySelector(".button-suivant");
 
-const questionForm1  = document.querySelector("container0");
-
-const containerQuestion2 = document.querySelector(".container-question1");
 
 // select timer from HTML file
 
-const timerElement = document.getElementById("file")
+//const timerElement = document.getElementById("file")
 
 // display all container
 
-console.log(container, question);
+//console.log(container, question);
 
-container.addEventListener("submit", (e) => {
+buttonOps.addEventListener("click", (e) => {
     e.preventDefault()
-    containerQuestion1.style.display = 'block'
-    containerQuestion2.style.display ='none'
-    container.style.display = 'none'
-    console(questionSecond)
-})
-
-questionForm1.addEventListener("submit", (e) => {
-    e.preventDefault()
-    containerQuestion2.style.display ='block'
-    containerQuestion1.style.display = 'none'
-    container.style.display = 'none'
- })
-
- questionForm2.addEventListener("submit", (e) => {
-    e.preventDefault()
-    containerQuestion2.style.display ='none'
-    containerQuestion1.style.display = 'none'
-    container.style.display = 'block'
-})
-containerQuestion1.addEventListener("submit", (e) => {
-    e.preventDefault()
-    containerQuestion2.style.display = 'block'
-    questionForm1.style.display = 'none'
+    containerQuestion.style.display = 'block'
     container.style.display = 'none'
 })
+
+buttonSuivant.addEventListener ("click", (e) => {
+    e.preventDefault()
+    containerQuestion.style.display = 'block'
+    container.style.display = 'none'
+    
+    const question = document.querySelector(input["Avec <script>", "on ouvre l'élément label", "on ferme HTML et on ouvre <script>", "Aucune bonne reponse"])
+
+})
+
+
+
+
+// questionForm1.addEventListener("submit", (e) => {
+//     e.preventDefault()
+//     containerQuestion2.style.display ='block'
+//     containerQuestion1.style.display = 'none'
+//     container.style.display = 'none'
+//  })
+
+//  questionForm2.addEventListener("submit", (e) => {
+//     e.preventDefault()
+//     containerQuestion2.style.display ='none'
+//     containerQuestion1.style.display = 'none'
+//     container.style.display = 'block'
+// })
+// containerQuestion1.addEventListener("submit", (e) => {
+//     e.preventDefault()
+//     containerQuestion2.style.display = 'block'
+//     questionForm1.style.display = 'none'
+//     container.style.display = 'none'
+// })
  
-// setting timer
+// // setting timer
 
-const departMinutes = 1
-let temps = departMinutes * 60
-setInterval(() => {
-  let minutes = parseInt(temps / 60, 10)
-  let secondes = parseInt(temps % 60, 10)
+// const departMinutes = 1
+// let temps = departMinutes * 60
+// setInterval(() => {
+//   let minutes = parseInt(temps / 60, 10)
+//   let secondes = parseInt(temps % 60, 10)
 
-  minutes = minutes < 10 ? "0" + minutes : minutes
-  secondes = secondes < 10 ? "0" + secondes : secondes
+//   minutes = minutes < 10 ? "0" + minutes : minutes
+//   secondes = secondes < 10 ? "0" + secondes : secondes
 
-  timerElement.innerText = `${minutes}:${secondes}`
-  temps = temps <= 0 ? 0 : temps - 1
-}, 1000)
+//   timerElement.innerText = `${minutes}:${secondes}`
+//   temps = temps <= 0 ? 0 : temps - 1
+// }, 1000)
 
 
 
