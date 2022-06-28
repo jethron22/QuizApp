7df92c7ddb2582e195ed2e9c818b574c8e9510b7
Jéthron Kashira <jethronbwira@gmail.com>
2022-06-28 09:25:58 +0200

    refactor: adding js timer

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const container = document.querySelector(".container");
-const question = document.querySelector("#container2");
+const question = document.querySelector("container2");
+
 
 const containerQuestion1 = document.querySelector(".container-question");
-const questionForm1  = document.querySelector("#container0");
+
+const questionForm1  = document.querySelector("container0");
 
 const containerQuestion2 = document.querySelector(".container-question1");
-const containerQuestion3 = document.querySelector(".container-question")
+
+// select timer from HTML file
+
+const timerElement = document.getElementById("file")
+
+// display all container
 
 console.log(container, question);
 
@@ -30,13 +37,27 @@ questionForm1.addEventListener("submit", (e) => {
     containerQuestion1.style.display = 'none'
     container.style.display = 'block'
 })
-containerQuestion3.addEventListener("submit", (e) => {
+containerQuestion1.addEventListener("submit", (e) => {
     e.preventDefault()
-    containerQuestion2.style.display ='none'
-    container.style.display = 'block'
-    containerQuestion1.style.display = 'none'
+    containerQuestion2.style.display = 'block'
+    questionForm1.style.display = 'none'
+    container.style.display = 'none'
 })
  
+// setting timer
+
+const departMinutes = 1
+let temps = departMinutes * 60
+setInterval(() => {
+  let minutes = parseInt(temps / 60, 10)
+  let secondes = parseInt(temps % 60, 10)
+
+  minutes = minutes < 10 ? "0" + minutes : minutes
+  secondes = secondes < 10 ? "0" + secondes : secondes
+
+  timerElement.innerText = `${minutes}:${secondes}`
+  temps = temps <= 0 ? 0 : temps - 1
+}, 1000)
 
 
 
